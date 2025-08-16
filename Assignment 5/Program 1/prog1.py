552a10bd797101798c94d47599394c142024b7c4
jeetsaha1 <jeet2022saha@gmail.com>
2025-08-16 05:35:25 +0530

    Assignment 5

---
+++ Assignment 5/Program 1/prog1.py
@@ -0,0 +1,13 @@
+# Write a python program to create a Double linked list along with the following functions:
+#    a) to display the elements of a Double Linked List.
+#    b) to add an element at beginning of a Double linked list.
+#    c) to add an element at the end of a Double linked list.
+#    d) to add an element at any user given position of a Double linked list.
+#    e) to search a user given element in a Double linked list.
+#    f) to count the number of nodes in a Double linked list.
+#    g) to delete a node at beginning of a Double linked list.
+#    h) to delete a node at end of a Double linked list.
+#    i) to delete a node from any position of a Double linked list.
+#    (Create a menu bar for accessing the functions)
+
+
